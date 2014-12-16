bdbd11fcfffb6d4f36ec1d4b2d1543b0d8d13ece
Eugene <Eugene@EugeneNotebook>
2014-12-16 22:17:06 +0600

    added evernote SDK + smaple code to authenticate in evernote sandbox

--- src/notes/Note.java
+++ src/notes/Note.java
@@ -1,6 +1,31 @@
 package notes;
 
+import java.util.List;
+
+import com.evernote.auth.*;
+import com.evernote.clients.*;
+import com.evernote.edam.error.EDAMSystemException;
+import com.evernote.edam.error.EDAMUserException;
+import com.evernote.edam.type.Notebook;
+import com.evernote.thrift.TException;
+
 public class Note {
-	private String sTring;
-	
+	public static void main(String[] args) throws EDAMUserException, EDAMSystemException,
+			TException {
+		// TODO: Get developerToken from config file, don't hardcode it
+		String developerToken = "...";
+		// Set up the NoteStore client
+		EvernoteAuth evernoteAuth = new EvernoteAuth(EvernoteService.SANDBOX,
+				developerToken);
+		ClientFactory factory = new ClientFactory(evernoteAuth);
+		NoteStoreClient noteStore = factory.createNoteStoreClient();
+
+		// Make API calls, passing the developer token as the
+		// authenticationToken param
+		List<Notebook> notebooks = noteStore.listNotebooks();
+
+		for (Notebook notebook : notebooks) {
+			System.out.println("Notebook: " + notebook.getName());
+		}
+	}
 }
